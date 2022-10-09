fccea18031ce28601db3a0c772ccfe1073321574
HungFai Koo <jhk888@yahoo.com>
2022-10-08 20:29:57 -0700

    sectionTwo

---
+++ Bus.java
@@ -0,0 +1,16 @@
+public class Bus {
+    public static void main(String[] args) {
+        String sentence = "Number of passengers: ";
+        System.out.println(sentence);
+        int passengers = 0;
+        passengers += 9;
+        System.out.println(sentence +passengers);
+
+        passengers  -= 5;
+        System.out.println(sentence + passengers);
+
+        passengers -= 4;
+        System.out.println(sentence + passengers);
+
+    }
+}
---
+++ WholeNumbers.java
@@ -0,0 +1,9 @@
+public class WholeNumbers {
+    public static void main(String[] args) {
+        int numberofFans = 5000;
+        System.out.println("There are " + numberofFans + " fans at the Quidditch games ");
+
+        long globalPopulation = 70000000000L;
+        System.out.println("There are " + globalPopulation + "people on earth");
+    }
+}
