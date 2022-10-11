eda1532e8190dcfda45743599d621d9400d761f1
HungFai Koo <jhk888@yahoo.com>
2022-10-10 20:24:15 -0700

    completed first respond app on java

---
+++ MathOperations.java
@@ -0,0 +1,23 @@
+public class MathOperations {
+    public static void main(String[] args) {
+        int bagOfSweets1 = 5;
+        int bagOfSweets2  = 10;
+
+        long starsInMilkyWay = 25000000000L;
+        long starsInAndromeda = 1000000000000L;
+
+        double testScore = 6.7;
+        double bonusMarks = 2.5;
+
+        System.out.println(bagOfSweets1 + bagOfSweets2);
+        System.out.println(starsInMilkyWay + starsInAndromeda);
+        System.out.println(testScore + bonusMarks);
+
+        int counter = 0;
+        counter++;
+        counter++;
+        counter++;
+        System.out.println(counter);
+
+    }
+}
