b075c075c2decde2f5e1e05905ab6e71d5ab37f1
HungFai Koo <jhk888@yahoo.com>
2022-10-17 12:28:30 -0700

    completed dealership app

--- Dealership.java
+++ Dealership.java
@@ -8,9 +8,44 @@ public class Dealership {
         System.out.println(" . Select option 'b' to buy a car");
         String option = scan.nextLine();
         switch(option) {
-            case "a": System.out.println("you chose option " + option); break;
-            case "b": System.out.println("you chose option " + option); break;
+            case "a": 
+            System.out.println("What is your budget?");
+            int budget = scan.nextInt();
+            if(budget >= 10000){
+                System.out.println("Great! A Nissan Altima is available");
+                System.out.println("\nDo you have insurance? Write 'yes or 'no'");
+                scan.nextLine();
+                String insurance = scan.nextLine();
+                System.out.println("\nDo you have a license? Write 'yes' or 'no;");
+                String license = scan.nextLine();
+                System.out.println("What is your credit score?");
+                int creditScore = scan.nextInt();
+                if(insurance.equals("yes") && license.equals("yes") && creditScore > 660) {
+                    System.out.println("Sold! Pleasure doing business with you");
+                } else {
+                    System.out.println("We're sorry. You are not eligible");
+                }
+            } else {
+                System.out.println("We don't sell cars under $10,000. Sorry!");
+            }
+            break;
+            case "b": 
+            System.out.println("What is your car valued at?");
+            int value = scan.nextInt();
+            System.out.println("What is your selling price?");
+            int price = scan.nextInt();
+            
+            if(value > price && price < 30000){
+                System.out.println("We will buy your car. Pleasure doing business with you!");
+            } else {
+                System.out.println("Sorry, we're not interested!");
+            }
+            break;
             default: System.out.println("Invalid option");
+
+            
+
+
             scan.close();
         }
     }
